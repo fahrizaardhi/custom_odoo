4992859ab9e86df0472ecdfa151bdbfa09ce49d3
fahrizaardhi <fahriardhi21@gmail.com>
2021-09-23 21:23:26 +0700

    first commit

---
+++ asb_send_message/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "ASB Send Message",
+    'summary': "To send messages",
+    'description': "This module used to send message when a user create PO",
+    'author': "PT. Arkana Solusi Bisnis",
+    'website': "http://www.arkana.co.id",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base', 'purchase'],
+    'data': ['data/mail_channel_data.xml'],
+}
---
+++ asb_send_message/models/models.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+    
+    # override fungsi button_confirm
+    def button_confirm(self):
+        res = super(PurchaseOrder, self).button_confirm()
+        for order in self:
+            # Jika user adalah anggota dari group purchase user 
+            # dan po_double_validation = two_step
+            # dan amount_total lebih besar atau sama dengan po_double_validation_amount
+            if order.user_has_groups('purchase.group_purchase_user')\
+                    and (order.company_id.po_double_validation == 'two_step'\
+                        and order.amount_total >= self.env.company.currency_id._convert(
+                            order.company_id.po_double_validation_amount, order.currency_id, order.company_id, order.date_order or fields.Date.today())):
+                # memanggil fungsi send_massage()
+               self.send_massage()
+        return res
+
+    # mengirim message
+    def send_massage(self):
+        # mendifinisikan record direct massage yang akan menerima pesan
+        direct_massage = self.env.ref('asb_send_message.purchase_order_room')
+        # mendefinisikan pesan yang akan dikirim
+        message = "PO Create, please check!"
+       # menambahkan pesan dengan link dokumen Purchase Order
+        body = message + " <a href='#id=%s&model=purchase.order&'>%s</a>" % (self.id, self.name)
+        for rec in direct_massage:
+            # mengirim message
+            rec.message_post(attachment_ids=[], body=body, content_subtype='html', message_type='comment', partner_ids=[], subtype_xmlid='mail.mt_comment')
+        return
